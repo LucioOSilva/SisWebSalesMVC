669cdaf258a7b6b70f6dd57e6e3ab341378cf35d
LucioOSilva <losalucard@hotmail.com>
2019-12-26 15:02:53 -0300

    CRUD Scaffolding

---
+++ SisWebSalesMVC/Data/SisWebSalesMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SisWebSalesMVC.Models
+{
+    public class SisWebSalesMVCContext : DbContext
+    {
+        public SisWebSalesMVCContext (DbContextOptions<SisWebSalesMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SisWebSalesMVC.Models.Department> Department { get; set; }
+    }
+}
