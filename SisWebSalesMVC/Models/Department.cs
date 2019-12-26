4d417c55cc4ced64a457d98a6cfb3f0423d7326d
LucioOSilva <losalucard@hotmail.com>
2019-12-26 16:59:33 -0300

    Second migration and other entities

--- SisWebSalesMVC/Models/Department.cs
+++ SisWebSalesMVC/Models/Department.cs
@@ -1,10 +1,30 @@
-﻿
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 namespace SisWebSalesMVC.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
+        public Department()
+        {
+        }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller Seller)
+        {
+            Sellers.Add(Seller);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(Seller => Seller.TotalSales(initial, final));
+        }
     }
 }
---
+++ SisWebSalesMVC/Models/SalesRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SisWebSalesMVC.Models.Enums;
+
+namespace SisWebSalesMVC.Models
+{
+    public class SalesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        public double Ammount { get; set; }
+
+        public SaleStatus Status { get; set; }
+
+        public Seller Seller { get; set; }
+
+        public SalesRecord()
+        {
+        }
+        public SalesRecord(int id, DateTime date, double ammount, SaleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Ammount = ammount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
