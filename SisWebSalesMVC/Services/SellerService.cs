63ac5497e61c95f5534f87ced9cafc9680884564
LucioOSilva <losalucard@hotmail.com>
2020-01-02 08:40:46 -0300

    Seller details and eager loading

--- SisWebSalesMVC/Services/SellerService.cs
+++ SisWebSalesMVC/Services/SellerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SisWebSalesMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SisWebSalesMVC.Services
 {
@@ -22,16 +23,16 @@ namespace SisWebSalesMVC.Services
         }
 
         public void Insert(Seller obj)
-        {            
+        {
             _context.Add(obj);
             _context.SaveChanges();
         }
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
-        public void Remove (int id)
+        public void Remove(int id)
         {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
