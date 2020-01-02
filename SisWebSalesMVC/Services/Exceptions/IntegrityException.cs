b97126911296c088d1ac8b24b8412a59c2f1068d
LucioOSilva <losalucard@hotmail.com>
2020-01-02 13:21:25 -0300

    Exception handling for delete (referencial integrity)

---
+++ SisWebSalesMVC/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SisWebSalesMVC.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+        }
+    }
+}
