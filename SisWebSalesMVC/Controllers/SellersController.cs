136919fb2b2365311f3a5ddaf2993c899e06dc12
LucioOSilva <losalucard@hotmail.com>
2020-01-02 09:29:51 -0300

    Update Seller and custom service exception

--- SisWebSalesMVC/Controllers/SellersController.cs
+++ SisWebSalesMVC/Controllers/SellersController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using SisWebSalesMVC.Services;
 using SisWebSalesMVC.Models;
 using SisWebSalesMVC.Models.ViewModels;
+using SisWebSalesMVC.Services.Exceptions;
 
 namespace SisWebSalesMVC.Controllers
 {
@@ -72,6 +73,42 @@ namespace SisWebSalesMVC.Controllers
             }
             return View(obj);
         }
-
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentService.Findall();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
--- SisWebSalesMVC/Services/SellerService.cs
+++ SisWebSalesMVC/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using SisWebSalesMVC.Models;
 using Microsoft.EntityFrameworkCore;
+using SisWebSalesMVC.Services.Exceptions;
 
 namespace SisWebSalesMVC.Services
 {
@@ -38,6 +39,21 @@ namespace SisWebSalesMVC.Services
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not Found");
+            }
+            try {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+            }
 
     }
 }
